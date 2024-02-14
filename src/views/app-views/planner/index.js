0adc28e8fadc51610a98b50c533665437bd9d4cf
svyatoslavkk <sv.krivonos1@gmail.com>
2024-02-15 00:57:43 +0300

    feat: add furniturelist

--- src/views/app-views/planner/index.js
+++ src/views/app-views/planner/index.js
@@ -1,23 +1,45 @@
 import React, { Component } from 'react';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
-import { Card, Space, Button } from 'antd';
+import { Card, Space, Button, Row, Col  } from 'antd';
+import circleTable from "../../../assets/img/circle-table.jpg";
+import squareTable from "../../../assets/img/square-table.jpg";
+import sofa from "../../../assets/img/sofa.jpg";
 
-const initialItems = Array.from({ length: 5 }, (v, k) => k).map(k => ({
+const initialItems = Array.from({ length: 3 }, (v, k) => k).map(k => ({
   id: `item-${k}`,
-  imageUrl: 'https://abrakadabra.fun/uploads/posts/2022-02/1644931101_13-abrakadabra-fun-p-stol-vid-sverkhu-20.jpg',
+  img: circleTable,
   order: k + 1
 }));
+
+const furnitureList = [
+  {
+    id: 1,
+    img: circleTable,
+    name: "Круглый стол с 8 сиденьями",
+  },
+  {
+    id: 2,
+    img: squareTable,
+    name: "Квадратный стол с 4 сиденьями",
+  },
+  {
+    id: 3,
+    img: sofa,
+    name: "Диван",
+  },
+];
 
 class Board extends Component {
   state = {
     items: initialItems
   };
 
-  addItem = () => {
+  handleAddItem = (id, img, name) => {
     const newItem = {
-      id: `item-${this.state.items.length}`,
-      imageUrl: 'https://abrakadabra.fun/uploads/posts/2022-02/1644931101_13-abrakadabra-fun-p-stol-vid-sverkhu-20.jpg',
-      order: this.state.items.length + 1
+      id: `item-${id}`,
+      img: img,
+      order: this.state.items.length + 1,
+      name: name 
     };
     this.setState(prevState => ({
       items: [...prevState.items, newItem]
@@ -54,103 +76,74 @@ class Board extends Component {
     return (
       <div>
         <h1>Planner</h1>
-        <Space>
-          <Button type="primary" onClick={this.addItem}>Добавить элемент</Button>
-          <Button type="danger" onClick={this.removeItem}>Удалить элемент</Button>
-          <Button type="danger" onClick={this.removeAllItems}>Удалить все</Button>
-        </Space>
-        <DragDropContext onDragEnd={this.onDragEnd}>
-          <div style={{ display: 'flex' }}>
-            {/* <div style={{ margin: '20px' }}>
-              <h2>Available Furniture</h2>
-              <Droppable droppableId="source">
-                {(provided) => (
-                  <div
-                    {...provided.droppableProps}
-                    ref={provided.innerRef}
-                    style={{ width: '150px' }}
-                  >
-                    {this.state.items.map(({ id, imageUrl, order }, index) => (
-                      <Draggable key={id} draggableId={id} index={index}>
-                        {(provided) => (
-                          <div
-                            ref={provided.innerRef}
-                            {...provided.draggableProps}
-                            {...provided.dragHandleProps}
-                            style={{
-                              width: '100px',
-                              height: '100px',
-                              margin: '10px',
-                              backgroundImage: `url(${imageUrl})`,
-                              backgroundSize: 'cover',
-                              backgroundPosition: 'center',
-                              display: 'flex',
-                              justifyContent: 'center',
-                              alignItems: 'center',
-                              ...provided.draggableProps.style
-                            }}
-                          >
-                            <span>{order}</span>
-                          </div>
-                        )}
-                      </Draggable>
-                    ))}
-                    {provided.placeholder}
+        <Row>
+          <Col style={{ display: 'flex', gap: '50px', flexDirection: 'column' }}>
+            <Row gutter={[16, 16]} style={{ gap: '16px' }}>
+              {furnitureList.map((el) => (
+                <Col key={el.id} style={{ width: '180px', height: '180px', gap: '24px' }}>
+                  <div onClick={() => this.handleAddItem(el.id, el.img, el.name)}> 
+                    <Card
+                      hoverable
+                      cover={<img alt="" src={el.img} style={{ width: '100%', height: '90%', objectFit: 'cover' }} />}
+                      bodyStyle={{ padding: '8px' }} 
+                    >
+                      <div style={{ fontSize: '12px', overflowWrap: 'break-word' }}>{el.name}</div>
+                    </Card>
                   </div>
-                )}
-              </Droppable>
-            </div> */}
-            <div style={{ margin: '20px' }}>
-              <h2>Placed Furniture</h2>
-              <Droppable droppableId="target">
-                {(provided) => (
-                  <div
-                    {...provided.droppableProps}
-                    ref={provided.innerRef}
-                    style={{ width: '400px', minHeight: '200px', border: '1px solid #ccc' }}
-                  >
-                    {this.state.items.map(({ id, imageUrl, order }, index) => (
-                      <Draggable key={id} draggableId={id} index={index}>
-                        {(provided) => (
-                          <>
-                          <div
-                            ref={provided.innerRef}
-                            {...provided.draggableProps}
-                            {...provided.dragHandleProps}
-                            style={{
-                              position: 'relative',
-                              width: '100px',
-                              height: '100px',
-                              margin: '10px',
-                              backgroundImage: `url(${imageUrl})`,
-                              backgroundSize: 'cover',
-                              backgroundPosition: 'center',
-                              display: 'flex',
-                              justifyContent: 'center',
-                              alignItems: 'center',
-                              zIndex: 1,
-                              ...provided.draggableProps.style
-                            }}
-                          >
-                            <h3
+                </Col>
+              ))}
+            </Row>
+            <Space>
+              <Button type="danger" onClick={this.removeItem}>Удалить элемент</Button>
+              <Button type="danger" onClick={this.removeAllItems}>Удалить все</Button>
+            </Space>
+          </Col>
+          <DragDropContext onDragEnd={this.onDragEnd}>
+            <div style={{ display: 'flex' }}>
+              <div style={{ margin: '20px' }}>
+                <Droppable droppableId="target">
+                  {(provided) => (
+                    <div
+                      {...provided.droppableProps}
+                      ref={provided.innerRef}
+                      style={{ width: '100%', minHeight: '200px', border: '1px solid #ccc' }}
+                    >
+                      {this.state.items.map(({ id, img, order }, index) => (
+                        <Draggable key={id} draggableId={id} index={index}>
+                          {(provided) => (
+                            <>
+                            <div
+                              ref={provided.innerRef}
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
                               style={{
-                                color: 'orange'
+                                position: 'relative',
+                                width: '100px',
+                                height: '100px',
+                                margin: '10px',
+                                backgroundImage: `url(${img})`,
+                                backgroundSize: 'cover',
+                                backgroundPosition: 'center',
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                zIndex: 1,
+                                ...provided.draggableProps.style
                               }}
                             >
-                              {order}
-                            </h3>
-                          </div>
-                          </>
-                        )}
-                      </Draggable>
-                    ))}
-                    {provided.placeholder}
-                  </div>
-                )}
-              </Droppable>
+                            </div>
+                            </>
+                          )}
+                        </Draggable>
+                      ))}
+                      {provided.placeholder}
+                    </div>
+                  )}
+                </Droppable>
+              </div>
             </div>
-          </div>
-        </DragDropContext>
+          </DragDropContext>
+        </Row>
       </div>
     );
   }
